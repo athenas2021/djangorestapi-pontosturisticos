f6d1fd583bcdd56fef4ce9ee01401c592263bcc8
athenas2021 <lucas.kano@outlook.com>
2021-09-01 19:18:46 -0300

    Adicionado modelo: Avaliacoes

---
+++ pontos_turisticos/avaliacoes/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Avaliacao(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comentario = models.TextField(null=True, blank=True)
+    nota = models.DecimalField(max_digits=3, decimal_places=1)
+    data = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
