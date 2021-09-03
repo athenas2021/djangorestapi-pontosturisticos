dfd5e5e36f64cba4d11962d53e850507cd6aeb69
athenas2021 <lucas.kano@outlook.com>
2021-09-03 12:10:36 -0300

    Adicionado rota para enderecos (/enderecos)

--- pontos_turisticos/pontos_turisticos/urls.py
+++ pontos_turisticos/pontos_turisticos/urls.py
@@ -19,10 +19,11 @@ from django.urls import path
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
-
+from enderecos.api.viewsets import EnderecoViewSet
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracaoViewSet)
+router.register(r'enderecos', EnderecoViewSet)
     
 
 urlpatterns = [
