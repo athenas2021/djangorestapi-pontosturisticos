6ff8ea7b416a1c31ff1be188a07660e17ed5064b
athenas2021 <lucas.kano@outlook.com>
2021-09-03 12:15:25 -0300

    Adicionado rota para comentarios (/comentarios)

--- pontos_turisticos/pontos_turisticos/urls.py
+++ pontos_turisticos/pontos_turisticos/urls.py
@@ -20,10 +20,14 @@ from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
+from comentarios.api.viewsets import ComentarioViewSet
+
 router = routers.DefaultRouter()
+
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracaoViewSet)
 router.register(r'enderecos', EnderecoViewSet)
+router.register(r'comentarios', ComentarioViewSet)
     
 
 urlpatterns = [
