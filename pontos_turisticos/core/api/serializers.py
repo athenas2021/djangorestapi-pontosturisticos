78ac42ad503aa56997844c54fb36dbb00bd3f99d
athenas2021 <lucas.kano@outlook.com>
2021-09-13 18:39:16 -0300

    Testes com SerializerMethodField no modelo Core. Testado no serializer.py (descricao_completa) e no models.py (descricao_completa2)

--- pontos_turisticos/core/api/serializers.py
+++ pontos_turisticos/core/api/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import  ModelSerializer
 from core.models import PontoTuristico
 from atracoes.api.serializers import AtracaoSerializer
@@ -6,8 +7,15 @@ from enderecos.api.serializers import EnderecoSerializer
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
     endereco = EnderecoSerializer()
+    # Campo adicional descricao_completa
+    descricao_completa = SerializerMethodField()
 
     class Meta:
         model = PontoTuristico
         fields = ('id','nome','descricao','foto',
-        'atracoes', 'comentarios', 'avaliacoes', 'endereco')
+        'atracoes', 'comentarios', 'avaliacoes', 'endereco',
+        'descricao_completa')
+
+    # Definindo o campo adicional
+    def get_descricao_completa(self, obj):
+        return '%s - %s' % (obj.nome, obj.descricao)
